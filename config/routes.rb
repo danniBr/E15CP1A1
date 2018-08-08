e7229872452f7388ffcc8140c61d7816d2b30d7f
danniBr <danitza.br@gmail.com>
2018-08-07 20:09:51 -0400

    new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :histories
+  get 'users/new'
 
+  resources :histories
   root "histories#index"
+  get 'users/sign_up', to: "users#new"
+  post 'users', to: "users#create"
+  
+  resources :sessions, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
