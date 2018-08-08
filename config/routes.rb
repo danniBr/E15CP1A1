472b1ae37c7a67aa7b6fc5acdbfb4cfd91d58a6d
danniBr <danitza.br@gmail.com>
2018-08-07 20:21:34 -0400

    iniciar y cerrar sesion

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   root "histories#index"
   get 'users/sign_up', to: "users#new"
   post 'users', to: "users#create"
-  
+
   resources :sessions, only: [:create, :destroy]
+  
+  get 'users/sign_in', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
